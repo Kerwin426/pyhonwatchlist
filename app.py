4c1fc8a7a1cff0b471090eeae04326695f8825da
gkm <2901997993@qq.com>
2024-09-17 01:11:47 +0800

    写到验证密码的散列值了，睡觉了

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,url_for,render_template,request,redirect,flash
 from markupsafe import escape
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 import os
 import sys
 import click
@@ -94,7 +95,12 @@ def initdb(drop):
 class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
     id = db.Column(db.Integer, primary_key=True)  # 主键
     name = db.Column(db.String(20))  # 名字
-
+    username = db.Column(db.String(20))  # 用户名
+    password_hash = db.Column(db.String(128))
+    def set_password(self,password):
+        self.password_hash = generate_password_hash(password)  # 密码
+    def validate_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
 class Movie(db.Model):  # 表名将会是 movie
     id = db.Column(db.Integer, primary_key=True)  # 主键
