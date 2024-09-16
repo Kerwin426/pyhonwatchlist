aab1b7d77a9caddb6e33940e4e852e2188eccafc
gkm <2901997993@qq.com>
2024-09-17 00:55:14 +0800

    Create, edit and delete item by form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,url_for,render_template
+from flask import Flask,url_for,render_template,request,redirect,flash
 from markupsafe import escape
 from flask_sqlalchemy import SQLAlchemy
 import os
@@ -14,6 +14,7 @@ else:
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型修改的监控
+app.config['SECRET_KEY'] = 'dev'  # 等同于 app.secret_key = 'dev'
 db = SQLAlchemy(app)# 初始化扩展，传入创建的 Flask 实例app
 
 
@@ -24,10 +25,49 @@ def inject_user():
     user = User.query.first()
     return dict(user=user)
 
-@app.route('/')
+@app.route('/',methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('index'))
+        movie = Movie(title=title, year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item created.')
+        return redirect(url_for('index'))
     movies = Movie.query.all()
     return render_template('index.html',  movies=movies)
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':  # 处理编辑表单的提交请求
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) != 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))  # 重定向回对应的编辑页面
+
+        movie.title = title  # 更新标题
+        movie.year = year  # 更新年份
+        db.session.commit()  # 提交数据库会话
+        flash('Item updated.')
+        return redirect(url_for('index'))  # 重定向回主页
+
+    return render_template('edit.html', movie=movie)  # 传入被编辑的电影记录
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])  # 限定只接受 POST 请求
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)  # 获取电影记录
+    db.session.delete(movie)  # 删除对应的记录
+    db.session.commit()  # 提交数据库会话
+    flash('Item deleted.')
+    return redirect(url_for('index'))  # 重定向回主页
 
 # 返回渲染好的错误模板
 @app.errorhandler(404)
@@ -89,33 +129,7 @@ def forge():
 
     db.session.commit()
     click.echo('Done.')
-# @app.route('/user/<name>')
-# def user_page(name):
-#     return f'User {escape(name)}'
 
-# @app.route('/test')
-# def test_url_for():
-#     # 下面的print是会在终端打印的
-#     print(url_for('hello')) # 生成URL
-#     print(url_for('user_page',name='greyli')) # 生成URL
-#     print(url_for('user_page',name='peter')) # 生成URL
-#     print(url_for('test_url_for')) # 生成URL
-#     print(url_for('test_url_for',num=2)) # 生成URL
-#     return 'This is a test page'
-
-# name = 'Kerwin Gu'
-# movies = [
-#     {'title': 'My Neighbor Totoro', 'year': '1988'},
-#     {'title': 'Dead Poets Society', 'year': '1989'},
-#     {'title': 'A Perfect World', 'year': '1993'},
-#     {'title': 'Leon', 'year': '1994'},
-#     {'title': 'Mahjong', 'year': '1996'},
-#     {'title': 'Swallowtail Butterfly', 'year': '1996'},
-#     {'title': 'King of Comedy', 'year': '1999'},
-#     {'title': 'Devils on the Doorstep', 'year': '1999'},
-#     {'title': 'WALL-E', 'year': '2008'},
-#     {'title': 'The Pork of Music', 'year': '2012'},
-# ]
 
 
 
