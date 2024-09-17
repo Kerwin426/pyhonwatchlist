b2fab1310166fba904a6af6525adcf882d014e8d
gkm <2901997993@qq.com>
2024-09-17 16:03:37 +0800

    Organize application with package

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,3 @@
 # .flaskenv 文件
 FLASK_DEBUG=0
+FLASK_APP=watchlist
---
+++ watchlist/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+# SQLite URI compatible
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型修改的监控
+app.config['SECRET_KEY'] = 'dev'  # 等同于 app.secret_key = 'dev'
+db = SQLAlchemy(app)# 初始化扩展，传入创建的 Flask 实例app
+login_manager = LoginManager(app)
+
+
+@login_manager.user_loader
+def load_user(user_id): # 创建用户加载回调函数，接受用户 ID 作为参数
+    from watchlist.models import User
+    user = User.query.get(int(user_id)) # 用 ID 去数据库查询用户
+    return user
+
+login_manager.login_view = 'login'
+# context_processor 装饰的函数返回的变量将会统一注入到每一个模板的上下文中，因此我们可以在模板中直接使用 user 变量
+@app.context_processor
+def inject_user():
+    from watchlist.models import User
+    user = User.query.first()
+    return dict(user=user)
+
+
+from watchlist import views, errors, commands
